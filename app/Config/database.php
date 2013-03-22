b5ce373b7922aea5c314f034dbb898b39cf6dc13
chancelab <k_shirato@chancelab.jp>
2013-03-22 17:58:26 +0900

    update gitignore file

---
+++ app/Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Postgres',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'columbus',
+		'password' => 'columbus',
+		'database' => 'columbus',
+	);
+}
